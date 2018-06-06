dc4cd5bfd76f9d5eb6308edfdbc5a33b63e4aef7
Ish Kumar Jain <ishjain1894@gmail.com>
2018-06-06 16:57:27 -0400

    minor

--- plotResults.m
+++ plotResults.m
@@ -1,4 +1,4 @@
-% #plotResults.m
+% #plotResults2.m for lower BL density
 % Take simData.csv and theoryData.csv and plot the results
 
 % allData = [pB,pBgiven,freq,freqCond,durCond];
@@ -10,24 +10,27 @@
 clear;
 close all;
 theory_pB=importdata('figures2/theory_pB.csv',',',1);
-theory_pBCond=importdata('figures2/theory_pBCond.csv',',',1);
+theory_pBCond=importdata('figures2/theory_pBCond2.csv',',',1);
 theory_freq=importdata('figures2/theory_freq.csv',',',1);
-theory_freqCond=importdata('figures2/theory_freqCond.csv',',',1);
-theory_durCond=importdata('figures2/theory_durCond.csv',',',1);
+theory_freqCond=importdata('figures2/theory_freqCond2.csv',',',1);
+theory_durCond=importdata('figures2/theory_durCond2.csv',',',1);
 
 sim_pB=importdata('figures2/sim_pB.csv',',',1);
-sim_pBCond=importdata('figures2/sim_pBCond.csv',',',1);
+sim_pBCond=importdata('figures2/sim_pBCond2.csv',',',1);
 sim_freq=importdata('figures2/sim_freq.csv',',',1);
-sim_freqCond=importdata('figures2/sim_freqCond.csv',',',1);
-sim_durCond=importdata('figures2/sim_durCond.csv',',',1);
+sim_freqCond=importdata('figures2/sim_freqCond2.csv',',',1);
+sim_durCond=importdata('figures2/sim_durCond2.csv',',',1);
 
 nlines = size(theory_pB.data,2)-1;
 
 color = {'r','g','b','m','k','c'};
 % legendArray = {'\lambda_B=0.1, \omega=0','\lambda_B=0.2, \omega=0',...
 %     '\lambda_B=0.1, \omega=2\pi/3','\lambda_B=0.2, \omega=2\pi/3'};
-legendArray= {'lamB0.01omega0','lamB0.1omega0','lamB0.2omega0',...
-    'lamB0.01omega60','lamB0.1omega60','lamB0.2omega60'};
+% legendArray= {'lamB0.01omega0','lamB0.1omega0','lamB0.2omega0',...
+%     'lamB0.01omega60','lamB0.1omega60','lamB0.2omega60'};
+
+legendArray= {'lamB0.005omega0','lamB0.01omega0',...
+    'lamB0.005omega60','lamB0.01omega60'};
 % h=figure(1);
 % grid on;
 % for j=1:nlines
